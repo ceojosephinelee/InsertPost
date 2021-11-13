142aeee46c8b159449cf1cb53fc77c13aa028fd1
sudaltokki <mary000605@naver.com>
2021-11-14 04:51:18 +0900

    show post

--- post/src/components/App.js
+++ post/src/components/App.js
@@ -1,20 +1,38 @@
 
-import React, {  useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import AppRouter from "./Router";
-
-import '../style/app.scss';
 import { authService } from "../fbase";
+import '../style/app.scss';
+
 
 function App() {
+  const [init, setInit] = useState(false); //초기화여부 확인
   const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   const [userObj, setuserObj] = useState("");
+  const [upload, setUpload] = useState(true);
+
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user){
+        setIsLoggedIn(true);
+        setuserObj(user.displayName);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+
   return (
     <div>
      <header>
        <Header isLoggedIn={isLoggedIn} userObj={userObj}/>
      </header>
-    <AppRouter isLoggedIn={isLoggedIn} userObj={userObj}></AppRouter>
+    { init ?
+      <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} upload={upload}></AppRouter>
+      : "Initialzing..."
+    }
     </div>
 
 
