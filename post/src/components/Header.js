66e26d5fc55717783ec56aa606a5c1645b47d7d3
이지현 <jose1123@naver.com>
2021-11-17 19:09:36 +0900

    반응형 update

--- post/src/components/Header.js
+++ post/src/components/Header.js
@@ -9,12 +9,13 @@ const Header = ({isLoggedIn}) => (
             <div class="container-fluid">
               <a class="navbar-brand" href="#">
                 <h1><Link to="/home">Insert Post</Link></h1></a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
+              
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                                          
+                
+                  {isLoggedIn ? <Link to="/profile"style={{textDecoration: 'none'}}><button type="button" class="btn btn-primary">Log out</button></Link> : <Link to="/signup"style={{textDecoration: 'none'}}>
+                    <button type="button" class="btn btn-primary">Log in</button>
+                  </Link>}                           
                 </ul>
                 <form className=" d-flex w-50">
                   <input className="form-control me-2" type="search" placeholder="입력하세요" aria-label="Search"></input>
